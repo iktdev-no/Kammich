9856a45941fa25fee4049bf6b2f0e6627938674f
Brage Skjønborg <brage@skjonborg.no>
2026-08-25 19:16:53 +0200

    SPA

--- src/main/kotlin/no/iktdev/kammich/controller/SpaController.kt
+++ src/main/kotlin/no/iktdev/kammich/controller/SpaController.kt
@@ -3,14 +3,12 @@ package no.iktdev.kammich.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class SpaController {
 
-    @RequestMapping(
-        value = ["/**"],
-        produces = ["text/html"]
-    )
+    @GetMapping(value = ["/{path:[^\\.]*}", "/**/{path:[^\\.]*}"])
     fun forward(): String {
         return "forward:/index.html"
     }
