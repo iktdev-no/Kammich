adffac51066b67829546e906445e8adab70ae5b1
Brage Skjønborg <brage@skjonborg.no>
2026-08-29 14:48:16 +0200

    Icon

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,43 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+
+    babel({
+      presets: [reactCompilerPreset()]
+    }),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Kammich",
+        short_name: "Kammich",
+        description: "Kammich",
+
+        theme_color: "#000000",
+        background_color: "#000000",
+
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
 
   server: {
